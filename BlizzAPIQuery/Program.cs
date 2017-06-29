c045dee22f1ba7595fa79e2e4b8931109ffda749
N. Dinio <nika1667@gmail.com>
2017-06-29 19:43:43 -0400

    Pushes everyhing into connected realms table. Aman'thul seems to throw exception due to ' character. Just needs escape character in the SQL query I think.

--- BlizzAPIQuery/Program.cs
+++ BlizzAPIQuery/Program.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -52,17 +53,33 @@ namespace BlizzAPIQuery
 			client.DefaultRequestHeaders.Accept.Clear();
 			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
+			RealmList realms = null;
+
 			try
 			{
-				RealmList realms = await GetRealmsAsync("realm/status?locale=en_US&apikey=k7rsncmwup6nttk6vzeg6knyw4jrjjzj");
+				realms = await GetRealmsAsync("realm/status?locale=en_US&apikey=k7rsncmwup6nttk6vzeg6knyw4jrjjzj");
+				Console.WriteLine("I've got the realm statuses!");
 			}
-			catch(Exception e)
+			catch (Exception e)
 			{
-				Console.WriteLine("Could not connect to the Blizz API to update realm status!");
+				Console.WriteLine("Could not connect to the Blizz API to update realm status!\n" + e.StackTrace);
+
+			}
+
+
+			try
+			{
+				InsertRealms(realms);
+				Console.WriteLine("Realms have been successfully updated!");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Could not insert realms into DB!\n" + e.StackTrace);
 			}
 
 			Console.ReadLine();
 		}
+
 
 		static async Task<RealmList> GetRealmsAsync(String path)
 		{
@@ -80,7 +97,88 @@ namespace BlizzAPIQuery
 
 			return realms;
 		}
+
+		static void InsertRealms(RealmList list)
+		{
+			String connectionString = "Data Source=(local);Initial Catalog=RealmData;"
+						+ "Integrated Security=SSPI;";
+
+			Realm[] realmArray = list.realms.ToArray();
+
+			
+
+			for (int i = 0; i < realmArray.Length; i++)
+			{
+				String[] crArray = realmArray[i].connected_realms.ToArray();
+
+				// First, handle connected realm listings.
+				// Just insert if only one in the connected realm list. 
+				if (crArray.Length == 1)
+					using (SqlConnection connection = new SqlConnection(connectionString))
+					{
+						SqlCommand command = new SqlCommand("INSERT INTO ConnectedRealms (RealmName) VALUES ('" + realmArray[i].slug +"');", connection);
+						command.Connection.Open();
+						command.ExecuteNonQuery();
+						command.Connection.Close();
+					}
+				else // else build a connect ID for all connected realms	
+					using (SqlConnection connection = new SqlConnection(connectionString))
+					{
+						SqlCommand command = new SqlCommand("SELECT ConnectID FROM ConnectedRealms WHERE RealmName = '" + realmArray[i].slug + "';", connection);
+						try
+						{
+							command.Connection.Open();
+							int result = (int)command.ExecuteScalar();
+						}
+						catch
+						{
+							command = new SqlCommand("INSERT INTO ConnectedRealms (RealmName) VALUES ('" + realmArray[i].slug + "');", connection);
+							command.ExecuteNonQuery();
+							command = new SqlCommand("SET IDENTITY_INSERT ConnectedRealms ON", connection);
+							command.ExecuteNonQuery();
+
+							command = new SqlCommand("SELECT ConnectID FROM ConnectedRealms WHERE RealmName = ('" + realmArray[i].slug + "');", connection);
+							int connectID = (int)command.ExecuteScalar();
+
+							for (int j = 0; j < crArray.Length; j++)
+								if(!crArray[j].Equals(realmArray[i].slug))
+								{
+									command = new SqlCommand("INSERT INTO ConnectedRealms (ConnectID, RealmName) VALUES (" + connectID +", '" + crArray[j] + "');", connection);
+									command.ExecuteNonQuery();
+								}
+
+							command = new SqlCommand("SET IDENTITY_INSERT ConnectedRealms OFF", connection);
+							command.ExecuteNonQuery();
+
+						}
+					}
+
+				// Finally, insert realm data using the connectIDs
+				using (SqlConnection connection = new SqlConnection(connectionString))
+				{
+					SqlCommand command = new SqlCommand("SELECT ConnectID FROM ConnectedRealms WHERE RealmName = '" + realmArray[i].slug + "';", connection);
+					command.Connection.Open();
+					int connectID = (int)command.ExecuteScalar();
+					int realmQueue = realmArray[i].queue ? 1 : 0;
+					int realmStatus = realmArray[i].status ? 1 : 0;
+					String realmName = realmArray[i].name;
+
+					if (realmName.Contains("'"));
+						realmName = realmName.
+
+					command = new SqlCommand("INSERT INTO RealmList (RealmType, RealmPop, RealmQueue, RealmStatus,"+ 
+						" RealmName, RealmSlug, Battlegroup, locale, timezone, ConnectID) VALUES('" + realmArray[i].type +
+						"', '" + realmArray[i].population + "', " + realmQueue + ", " + realmStatus + 
+						", '" + realmName + "', '" + realmArray[i].slug + "', '" + realmArray[i].battlegroup + 
+						"', '" + realmArray[i].locale + "', '" + realmArray[i].timezone + "', " + connectID + ");", connection);
+
+					command.ExecuteNonQuery();
+				}
+
+			}
+		}
 	}
+
 
 
 }

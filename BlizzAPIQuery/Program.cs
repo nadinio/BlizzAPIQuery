305dd119eacc2314003ef35d0b7564976cd48991
N. Dinio <nika1667@gmail.com>
2017-07-14 00:32:48 -0400

    Based command line functionality. Update to console output.

--- BlizzAPIQuery/Program.cs
+++ BlizzAPIQuery/Program.cs
@@ -6,22 +6,38 @@ namespace BlizzAPIQuery
 	{
 		static void Main(string[] args)
 		{
-			/*while (true)
-			{ 
+			while (true)
+			{
+				Console.WriteLine("Commmands: \nrl \t\t Update the realmlist.\nahupdate \t Downloads and updates all AH data.");
 				String userInput = Console.ReadLine();
-				Console.Write(userInput);
-			}*/
+				switch(userInput)
+				{
+					case "rl":
+						RealmListQuery realmList = new RealmListQuery();
+						realmList.updateRealmList();
+						break;
+					case "ahupdate":
+						AuctionHouseQuery ahData = new AuctionHouseQuery();
+						ahData.updateAHData();
+						break;
+					default:
+						Console.WriteLine("Invalid command.");
+						break;
+
+				}
+			}
 
 			/*
 			RealmListQuery realmList = new RealmListQuery();
 			realmList.updateRealmList();
-			*/
+			
 
 			AuctionHouseQuery ahData = new AuctionHouseQuery();
 			ahData.updateAHData();
+			*/
 
 
-			Console.ReadLine();
+
 		}	
 	}
 }
